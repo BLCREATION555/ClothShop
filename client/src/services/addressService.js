9878aea4b75374642b7a08ffcea408ee13a74ef2
BLCREATION555 <kirtenpiyush123@gmail.com>
2026-08-05 18:49:22 +0530

    Fix address service API

--- client/src/services/addressService.js
+++ client/src/services/addressService.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://blcreation-api.onrender.com/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 API.interceptors.request.use((config) => {
