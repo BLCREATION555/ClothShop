8b982cc5479711ea2eb347c71273fcb850c7772e
BLCREATION555 <kirtenpiyush123@gmail.com>
2026-08-08 12:20:36 +0530

    fix uploads path

--- server/src/app.js
+++ server/src/app.js
@@ -49,7 +49,7 @@ app.use(
 
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true }));
-app.use("/uploads", express.static("uploads"));
+app.use("/uploads", express.static(__dirname + "/../uploads"));
 
 /*
 |--------------------------------------------------------------------------
